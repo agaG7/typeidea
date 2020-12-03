9573e92944c441b1a89060ceb3854d928d2db04b
agaG7 <739078657@qq.com>
2020-12-03 14:41:55 +0800

    “添加你的注zhushi

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,10 @@
+from .base import *  # NOQA
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
